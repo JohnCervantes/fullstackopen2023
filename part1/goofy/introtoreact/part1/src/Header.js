42ae7de96c4f9be542c8461be63cfd8afc56cbef
john Cervantes <johncamilocervantes@gmail.com>
2023-02-27 10:02:48 -0600

    first commit

---
+++ part1/goofy/introtoreact/part1/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header({course}) {
+  return (
+    <div>{course}</div>
+  )
+}
+
+export default Header
---
+++ part1/goofy/introtoreact/part1/src/Total.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Total({exercises1, exercises2, exercises3}) {
+  return (
+    <div>Number of exercises {exercises1 + exercises2 + exercises3}</div>
+  )
+}
+
+export default Total
